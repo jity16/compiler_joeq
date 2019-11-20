d3d89054377c92a7d03cb2021a8fb84f2c4f526a
jity16 <jity16@mails.tsinghua.edu.cn>
2019-11-20 17:23:25 +0800

    exp2

---
+++ exp2/exp2/src/submit/FindRedundantNullChecks.java
@@ -0,0 +1,24 @@
+package submit;
+
+import java.util.*;
+
+public class FindRedundantNullChecks {
+
+    /**
+     * Main method of FindRedundantNullChecks.
+     * This method should print out a list of quad ids of redundant null checks for each function.
+     * The format should be "method_name id0 id1 id2", integers for each id separated by spaces.
+     *
+     * @param args an array of class names
+     */
+    public static void main(String[] args) {
+        // TODO: Fill in this, like:
+        //FlowSolver solver = new FlowSolver();
+        //for (String name : args) {
+        //    jq_Class clazz = (jq_Class) Helper.load(name);
+        //    Analysis analysis = new PrintRedundant();
+        //    solver.registerAnalysis(analysis);
+        //    Helper.runPass(clazz, solver);
+        //}
+    }
+}
