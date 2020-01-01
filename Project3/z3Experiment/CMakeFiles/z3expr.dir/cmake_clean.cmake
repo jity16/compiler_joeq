b28e14b141c0f8be57bd6d4ef32ed9f252ea0e64
jity16 <jity16@mails.tsinghua.edu.cn>
2020-01-01 18:45:28 +0800

    z3

---
+++ Project3/z3Experiment/CMakeFiles/z3expr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/z3expr.dir/main.cpp.o"
+  "z3expr"
+  "z3expr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/z3expr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
